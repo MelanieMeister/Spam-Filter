944528cd6bbd74296a9426be3602a42af4c0ebd4
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2019-10-07 15:54:32 +0200

    SpamKorpus getter/setter

--- src/main/java/src/ch/brugg/fhwn/dto/SpamKorpus.java
+++ src/main/java/src/ch/brugg/fhwn/dto/SpamKorpus.java
@@ -6,4 +6,12 @@ import java.util.List;
 public class SpamKorpus {
 
     private List<Email> spamMails= new ArrayList<>();
+
+    public List<Email> getSpamMails() {
+        return spamMails;
+    }
+
+    public void setSpamMails(List<Email> spamMails) {
+        this.spamMails = spamMails;
+    }
 }
